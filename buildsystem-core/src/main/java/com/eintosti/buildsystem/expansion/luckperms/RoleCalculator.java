8a3849526368fc8e8dd10403538ced4009b3e5f2
Thomas Meaney <thomas.meaney@icloud.com>
2022-02-17 17:34:25 +0100

    fix: Project not building
    Signed-off-by: Thomas Meaney <thomas.meaney@icloud.com>

--- buildsystem-core/src/main/java/com/eintosti/buildsystem/expansion/luckperms/RoleCalculator.java
+++ buildsystem-core/src/main/java/com/eintosti/buildsystem/expansion/luckperms/RoleCalculator.java
@@ -17,6 +17,7 @@ import net.luckperms.api.context.ContextSet;
 import net.luckperms.api.context.ImmutableContextSet;
 import org.bukkit.entity.Player;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
@@ -39,6 +40,7 @@ public class RoleCalculator implements ContextCalculator<Player> {
         contextConsumer.accept(KEY, Role.matchRole(player, buildWorld).toString());
     }
 
+    @NotNull
     @Override
     public ContextSet estimatePotentialContexts() {
         ImmutableContextSet.Builder builder = ImmutableContextSet.builder();
