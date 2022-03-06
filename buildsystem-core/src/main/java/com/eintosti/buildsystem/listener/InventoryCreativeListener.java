4e25d7c8eb9290a80e164a261dbef17a60a70c8e
Thomas Meaney <thomas.meaney@icloud.com>
2022-03-06 13:06:58 +0100

    Remove unneeded imports
    Signed-off-by: Thomas Meaney <thomas.meaney@icloud.com>

--- buildsystem-core/src/main/java/com/eintosti/buildsystem/listener/InventoryCreativeListener.java
+++ buildsystem-core/src/main/java/com/eintosti/buildsystem/listener/InventoryCreativeListener.java
@@ -17,11 +17,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCreativeEvent;
 import org.bukkit.event.inventory.InventoryType;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
