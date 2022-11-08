efdd93acbad3d1398b5f5706408478f0831efdd4
Thomas Meaney <thomas.meaney@icloud.com>
2022-11-08 11:38:05 +0100

    Remove blank line after copyright
    Signed-off-by: Thomas Meaney <thomas.meaney@icloud.com>

--- buildsystem-core/src/main/java/com/eintosti/buildsystem/command/subcommand/worlds/SetItemSubCommand.java
+++ buildsystem-core/src/main/java/com/eintosti/buildsystem/command/subcommand/worlds/SetItemSubCommand.java
@@ -5,7 +5,6 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
 package com.eintosti.buildsystem.command.subcommand.worlds;
 
 import com.cryptomorin.xseries.XMaterial;
