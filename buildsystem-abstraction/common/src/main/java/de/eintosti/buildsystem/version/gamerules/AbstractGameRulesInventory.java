f481c91bc65f2bfd764f169c7ec1584d2330f0b8
Thomas Meaney <thomas.meaney@icloud.com>
2023-06-18 15:38:37 +0200

    Switch to GNU v3 license (#202)
    [ci skip] Change to GNU v3 license

--- buildsystem-abstraction/common/src/main/java/de/eintosti/buildsystem/version/gamerules/AbstractGameRulesInventory.java
+++ buildsystem-abstraction/common/src/main/java/de/eintosti/buildsystem/version/gamerules/AbstractGameRulesInventory.java
@@ -1,9 +1,19 @@
 /*
- * Copyright (c) 2023, Thomas Meaney
- * All rights reserved.
+ * Copyright (c) 2018-2023, Thomas Meaney
+ * Copyright (c) contributors
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 package de.eintosti.buildsystem.version.gamerules;
 
