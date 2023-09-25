287407cdb5fbadfd9abd2280fb131cb1821b20d5
Thomas Meaney <thomas.meaney@icloud.com>
2023-09-25 09:43:39 +0200

    Add support for 1.20.2

--- buildsystem-core/src/main/java/de/eintosti/buildsystem/internal/ServerVersion.java
+++ buildsystem-core/src/main/java/de/eintosti/buildsystem/internal/ServerVersion.java
@@ -46,6 +46,7 @@ public enum ServerVersion {
     v1_19_R2(3218, CustomBlocks_1_17_R1.class, GameRules_1_13_R1.class),
     v1_19_R3(3337, CustomBlocks_1_17_R1.class, GameRules_1_13_R1.class),
     v1_20_R1(3465, CustomBlocks_1_20_R1.class, GameRules_1_13_R1.class),
+    v1_20_R2(3578, CustomBlocks_1_20_R1.class, GameRules_1_13_R1.class),
     UNKNOWN;
 
     private final int dataVersion;
