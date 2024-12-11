93b9b94870b825be009042ecfa905e0292e16cb9
EinJOJO <admin@einjojo.it>
2024-12-11 14:41:44 +0100

    removed empty space

--- buildsystem-core/src/main/java/de/eintosti/buildsystem/BuildSystem.java
+++ buildsystem-core/src/main/java/de/eintosti/buildsystem/BuildSystem.java
@@ -335,7 +335,6 @@ public class BuildSystem extends JavaPlugin {
         new SignChangeListener(this);
         new WeatherChangeListener(this);
         new WorldManipulateListener(this);
-
     }
 
     private void registerStats() {
