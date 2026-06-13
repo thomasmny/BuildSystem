3f6c8ef073a964687d7e08b8ad6b7e274621339f
Thomas Meaney <thomas.meaney@icloud.com>
2026-06-13 16:45:16 +0200

    build: add foojay toolchain resolver for Java 25 auto-provisioning
    Gradle cannot find Java 25 on the Jenkins Linux agent. Adding the
    foojay-resolver-convention plugin to both settings files lets Gradle
    auto-download JDK 25 from Adoptium without manual server configuration.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+}
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
